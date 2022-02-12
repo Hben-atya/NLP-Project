6cb7ff6013434e21c777287df85df208eb6e8a2d
Hben-atya <73015984+Hben-atya@users.noreply.github.com>
2022-02-12 14:38:38 +0200

    Create project_flow.py

---
+++ project_flow.py
@@ -0,0 +1,27 @@
+"""
+
+medical VQA project - methods:
+
+1. image encoders: 
+  a. VGG 
+  b. ResNet (EfficientNet ?)
+  c. MOCO - self supervised
+  d. VAE 
+  
+2. language encoders:
+  a. BERT
+  b. BioBERT
+  c. BioMed-RoBERTa-base
+
+3. Fusion Algorithm:
+  a. attention mechanism:
+    i.   Stacked At-tention Networks (SAN) 
+    ii.  Bilinear Attention Networks (BAN)
+    iii. transformer?
+  b. Multi-modal pooling:
+    i.   direct concatenation
+    ii.  
+
+
+
+"""
